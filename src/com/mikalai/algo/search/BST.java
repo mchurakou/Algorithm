742e84c3fe653edc332e9a0a94a7a34f1794f4e6
mchurakou <1badbug1@gmail.com>
2015-06-04 11:12:19 +0300

    - BST additional methods

--- src/com/mikalai/algo/search/BST.java
+++ src/com/mikalai/algo/search/BST.java
@@ -71,14 +71,155 @@ public class BST<Key extends Comparable<Key>, Value> {
         return x;
     }
 
+    public Key min(){
+        return min(root).key;
+    }
+
+    private Node min(Node root) {
+        if (root.left == null) return root;
+        return min(root.left);
+
+    }
+
+    public Key floor(Key key){
+        Node x = floor(root, key);
+        if (x == null) return null;
+        return x.key;
+    }
+
+    private Node floor(Node x, Key key) {
+        if (x == null) return null;
+
+        int cmp = key.compareTo(x.key);
+
+        if (cmp == 0) return x;
+
+        if (cmp < 0) return floor(x.left, key);
+
+        Node t = floor(x.right, key);
+        if (t!= null) return t;
+        else
+            return x;
+    }
+
+    public Key select(int k){
+        return select(root, k).key;
+    }
+
+    private Node select(Node x, int k) {
+        if (x == null) return null;
+
+        int t = size(x.left);
+
+        if (t >  k)
+            return select(x.left, k);
+        else if (t < k){
+            return select(x.right, k - t - 1);
+        } else {
+            return x;
+        }
+
+    }
+
+    public int rank(Key key){
+        return rank(key, root);
+    }
+
+    private int rank(Key key, Node x) {
+        if (x == null) return 0;
+
+        int cmp = key.compareTo(x.key);
+
+        if (cmp < 0){
+            return rank(key, x.left);
+        } else if (cmp > 0){
+            return 1 + size(x.left) + rank(key, x.right);
+        } else {
+            return size(x.left);
+        }
+    }
+
+    public void deleteMin(){
+
+        root  = deleteMin(root);
+    }
+
+    private Node deleteMin(Node x){
+        if (x.left == null) return x.right;
+
+        x.left = deleteMin(x.left);
+        x.N = size(x.left) + size(x.right) + 1;
+
+        return x;
+    }
+
+    public void delete(Key key){
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, Key key){
+        if (x == null) return null;
+
+        int cmp  = key.compareTo(x.key);
+
+        if (cmp < 0) x.left = delete(x.left, key);
+        else if (cmp > 0 ) x.right = delete(x.right, key);
+        else {
+            if (x.right == null) return x.left;
+
+            if (x.left == null) return x.right;
+
+            Node t = x;
+
+            x = min(t.right);
+
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+
+        x.N = size(x.left) + size(x.right)  + 1;
+        return x;
+
+    }
+
+    public void print(){
+        print(root);
+    }
+
+    private void print(Node x){
+        if (x == null) return;
+
+        print(x.left);
+        System.out.println(x.key);
+        print(x.right);
+    }
+
     public static void main(String[] args) {
         BST map = new BST<String, String>();
-        map.put("s1", "s11");
-        map.put("s2", "s22");
-        map.put("s2", "s33");
+        map.put("1", "s11");
+        map.put("2", "s22");
+        map.put("3", "s33");
 
-        System.out.println(map.get("s1"));
-        System.out.println(map.get("s2"));
+        System.out.println("pr");
+        map.print();
+        System.out.println("pr");
+
+        System.out.println(map.get("1"));
+        System.out.println(map.get("2"));
+
+        System.out.println(map.min());
+        System.out.println(map.floor("23"));
+
+        System.out.println(map.select(1));
+
+        System.out.println(map.rank("0"));
+
+        map.deleteMin();
+
+
+
+        map.delete("3");
+        System.out.println(map.min());
     }
 
 
