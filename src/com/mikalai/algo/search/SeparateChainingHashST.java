070099960426ed80413f9539321f386cd784be08
mchurakou <1badbug1@gmail.com>
2015-06-15 23:10:27 +0300

    -Separate Chaining Hash

---
+++ src/com/mikalai/algo/search/SeparateChainingHashST.java
@@ -0,0 +1,50 @@
+package com.mikalai.algo.search;
+
+/**
+ * Created by mikalai on 15.06.2015.
+ */
+public class SeparateChainingHashST<Key, Value> {
+
+    private int N;
+    private int M;
+    private SequatialSearchST<Key, Value>[] st;
+
+    public SeparateChainingHashST(int m) {
+        M = m;
+
+        st = (SequatialSearchST<Key, Value>[]) new SequatialSearchST[M];
+        for (int i = 0; i < st.length; i++) {
+            st[i] = new SequatialSearchST();
+
+        }
+    }
+
+    public SeparateChainingHashST() {
+        this(997);
+    }
+
+    private int hash(Key key) {
+        return key.hashCode() & 0x7fffffff % M;
+    }
+
+    public Value get(Key key) {
+        return st[hash(key)].get(key);
+    }
+
+    public void put(Key key, Value value) {
+        st[hash(key)].put(key, value);
+    }
+
+    public static void main(String[] args) {
+        SeparateChainingHashST<String, String> map = new SeparateChainingHashST<String, String>();
+
+        map.put("1", "1");
+        map.put("2", "2");
+        map.put("2", "3");
+
+        System.out.println(map.get("1"));
+        System.out.println(map.get("2"));
+
+
+    }
+}
