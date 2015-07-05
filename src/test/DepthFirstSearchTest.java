87d3a2fe72137b8b50067b3ad37f3e7efece4d54
mchurakou <1badbug1@gmail.com>
2015-07-05 23:25:56 +0300

    -ant tests

---
+++ src/test/DepthFirstSearchTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import junit.framework.Assert;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+
+/**
+ * Created by mikalai on 05.07.2015.
+ */
+
+@RunWith(JUnit4.class)
+public class DepthFirstSearchTest {
+
+    @org.junit.Test
+    public void testCount() throws Exception {
+        System.out.println("123");
+        org.junit.Assert.assertTrue(true);
+    }
+}
