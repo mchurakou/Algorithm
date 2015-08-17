5cb7b5ac37413a53459ee069cc45a4a8907e2962
mchurakou <1badbug1@gmail.com>
2015-08-17 23:48:19 +0300

    -orgraph dfs

---
+++ src/com/mikalai/algo/graph/orgraph/Digraph.java
@@ -0,0 +1,56 @@
+package com.mikalai.algo.graph.orgraph;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * Created by mikalai on 17.08.2015.
+ */
+public class Digraph {
+    private final int V;
+    private int E;
+    private Set<Integer>[] adj;
+
+    public Digraph(int v) {
+        V = v;
+        this.E = 0;
+
+        adj = (Set<Integer>[]) new Set[V];
+
+        for (int i = 0; i < V; i++) {
+            adj[i] = new LinkedHashSet<Integer>();
+
+        }
+
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+
+        E++;
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    public Digraph reverse() {
+        Digraph R = new Digraph(V);
+        for (int v = 0; v < V; v++) {
+            for (int w : adj(v)) {
+                R.addEdge(w, v);
+            }
+
+        }
+
+        return R;
+    }
+}
---
+++ src/com/mikalai/algo/graph/orgraph/DirectedDFS.java
@@ -0,0 +1,53 @@
+package com.mikalai.algo.graph.orgraph;
+
+import com.mikalai.algo.graph.Graph;
+
+import java.util.Stack;
+
+/**
+ * Created by mikalai on 17.08.2015.
+ */
+public class DirectedDFS {
+
+    private boolean[] marked;
+
+    public DirectedDFS(Digraph g, int s) {
+        marked = new boolean[g.V()];
+        dfs(g, s);
+    }
+
+    private void dfs(Digraph g, int v) {
+        marked[v] = true;
+
+        for (int w : g.adj(v)) {
+            if (!marked[w]) {
+                dfs(g, w);
+            }
+
+        }
+    }
+
+    public boolean marked(int w) {
+        return marked[w];
+    }
+
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+
+    public static void main(String[] args) {
+        Digraph g = new Digraph(6);
+
+        g.addEdge(0, 1);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
+        g.addEdge(4, 5);
+
+        DirectedDFS dps = new DirectedDFS(g, 0);
+
+        System.out.println(dps.hasPathTo(5));
+
+    }
+}
