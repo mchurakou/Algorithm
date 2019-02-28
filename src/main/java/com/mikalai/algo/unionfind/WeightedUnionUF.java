833e4f78d847744faf7ce60b0d6a78b388eef2c3
mchurakou <1badbug1@gmail.com>
2019-02-28 22:15:21 +0300

    -naming

--- src/main/java/com/mikalai/algo/unionfind/WeightedUnionUF.java
+++ src/main/java/com/mikalai/algo/unionfind/WeightedUnionUF.java
@@ -4,17 +4,17 @@ package com.mikalai.algo.unionfind;
  * Created by mikalai on 16.04.2015.
  */
 public class WeightedUnionUF {
-    private int[] id;
-    private int[] sz;
+    private int[] parentIds;
+    private int[] componentSize;
     private int count;
 
     public WeightedUnionUF(int count) {
         this.count = count;
-        id = new int[count];
-        sz = new int[count];
+        parentIds = new int[count];
+        componentSize = new int[count];
         for (int i = 0; i < count; i++) {
-            id[i] = i;
-            sz[i] = 1;
+            parentIds[i] = i;
+            componentSize[i] = 1;
         }
     }
 
@@ -23,28 +23,28 @@ public class WeightedUnionUF {
     }
 
     public boolean connected(int p, int q) {
-        return find(p) == find(q);
+        return findRoot(p) == findRoot(q);
     }
 
-    private int find(int p) {
-        while (p != id[p])
-            p = id[p];
+    private int findRoot(int p) {
+        while (p != parentIds[p])
+            p = parentIds[p];
 
         return p;
     }
 
     public void union(int p, int q) {
-        int pRoot = find(p);
-        int qRoot = find(q);
+        int pRoot = findRoot(p);
+        int qRoot = findRoot(q);
 
         if (pRoot == qRoot) return;
 
-        if (sz[pRoot] < sz[qRoot]) {
-            id[pRoot] = qRoot;
-            sz[qRoot] += sz[pRoot];
+        if (componentSize[pRoot] < componentSize[qRoot]) {
+            parentIds[pRoot] = qRoot;
+            componentSize[qRoot] += componentSize[pRoot];
         } else {
-            id[qRoot] = pRoot;
-            sz[pRoot] += sz[qRoot];
+            parentIds[qRoot] = pRoot;
+            componentSize[pRoot] += componentSize[qRoot];
         }
 
         count--;
